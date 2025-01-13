c36a0e634e4ad75b6a9f15d48575276fd98e2ef2
Egor Kuznetsov <egorkuz1408@gmail.com>
2025-01-13 11:48:34 +0400

    Created timer for waiting fragment

--- app/src/main/java/com/kzerk/fitnessapp/fragments/ExerciseListFragment.kt
+++ app/src/main/java/com/kzerk/fitnessapp/fragments/ExerciseListFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kzerk.fitnessapp.adapters.ExerciseAdapter
 import com.kzerk.fitnessapp.databinding.ExerciseListBinding
+import com.kzerk.fitnessapp.utils.FragmentManager
 import com.kzerk.fitnessapp.utils.MainViewModel
 
 
@@ -37,6 +39,9 @@ class ExerciseListFragment: Fragment() {
 		adapter = ExerciseAdapter()
 		rcView.layoutManager = LinearLayoutManager(activity)
 		rcView.adapter = adapter
+		bStart.setOnClickListener {
+			FragmentManager.setFragment(WaitingFragment.newInstance(), activity as AppCompatActivity)
+		}
 	}
 
 	companion object {
---
+++ app/src/main/java/com/kzerk/fitnessapp/fragments/WaitingFragment.kt
@@ -0,0 +1,60 @@
+package com.kzerk.fitnessapp.fragments
+
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kzerk.fitnessapp.adapters.ExerciseAdapter
+import com.kzerk.fitnessapp.databinding.ExerciseListBinding
+import com.kzerk.fitnessapp.databinding.WaitingFragmentBinding
+import com.kzerk.fitnessapp.utils.MainViewModel
+import com.kzerk.fitnessapp.utils.TimeUtils
+
+const val COUNT_DOWN_TIME = 11000L
+class WaitingFragment: Fragment() {
+	private lateinit var binding: WaitingFragmentBinding
+	private lateinit var timer: CountDownTimer
+
+
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		binding = WaitingFragmentBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.pBar.max = COUNT_DOWN_TIME.toInt()
+		startTimer()
+	}
+
+	private fun startTimer() = with(binding) {
+		timer = object : CountDownTimer(COUNT_DOWN_TIME, 1) {
+			override fun onTick(restTime: Long) {
+				tvTimer.text = TimeUtils.getTime(restTime)
+				pBar.progress = restTime.toInt()
+			}
+
+			override fun onFinish() {
+
+			}
+
+		}.start()
+	}
+
+	override fun onDetach() {
+		super.onDetach()
+		timer.cancel()
+	}
+
+	companion object {
+		@JvmStatic
+		fun newInstance() = WaitingFragment()
+	}
+}
